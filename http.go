6ebe2e740e5b7e42fd5b22c22157601e94c6837c
Jesús Boca <jbl_13@hotmail.com>
2016-04-23 20:53:16 +0200

    Parte http antes de cambiar lista por map

--- http.go
+++ http.go
@@ -8,7 +8,10 @@ import (
 	//"mime/multipart"
 	"net/http"
 	//"os"
+	"container/list"
 	"strconv"
+
+	"github.com/valyala/gorpc"
 )
 
 import _ "image/png"
@@ -17,16 +20,26 @@ import _ "image/jpeg"
 
 // Nodo
 type Nodo struct {
-	ip        string
+	//ip        string
 	idtrabajo int
+	cliente   *gorpc.DispatcherClient
+	resultado byte
 }
 
-func (n *Nodo) AsginarTrabajo(id int, imagen image.Image) bool {
+func (n *Nodo) AsignarTrabajo(id int, imagen *image.YCbCr) bool {
 	if n.idtrabajo == -1 {
-		n.idtrabajo = id
 		// Call RPC
-
-		return true
+		res, err := n.cliente.Call("RecibeImagen", imagen)
+		fmt.Print(res)
+		if err != nil {
+			n.idtrabajo = id
+			n.resultado = 0
+			//i := imagen.(image.YCbCr)
+			fmt.Print(imagen)
+			return true
+		} else {
+			return false
+		}
 	} else {
 		return false
 	}
@@ -34,13 +47,13 @@ func (n *Nodo) AsginarTrabajo(id int, imagen image.Image) bool {
 
 var id int = 0
 
-//nodos list
+var nodos *list.List
+var indexRobin int = 0
 
 func handler_subir(w http.ResponseWriter, r *http.Request) {
 	// Recibir archivo
 	id++
 
-	//archivo, header, err := r.FormFile("archivo")
 	reader, err := r.MultipartReader()
 
 	if err != nil {
@@ -59,6 +72,9 @@ func handler_subir(w http.ResponseWriter, r *http.Request) {
 	}
 
 	imagen, _, err := image.Decode(parte)
+	img := imagen.(*image.YCbCr)
+	otraimg := image.Image(img)
+	fmt.Print(otraimg)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintln(w, "Error del servidor:")
@@ -67,9 +83,21 @@ func handler_subir(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Pasarle la image a otro método
-	// Quien?
-	var nodo Nodo
-	if nodo.AsginarTrabajo(id, imagen) == true {
+	// Decidir a quien. Round-robin
+	e := nodos.Front()
+	for i := 0; (e != nil) && (i < indexRobin); e = e.Next() {
+	}
+	if e == nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		fmt.Fprintln(w, "Todos los nodos están ocupados")
+		indexRobin = 0
+		return
+	}
+	var nodo Nodo = e.Value.(Nodo)
+
+	nodo.idtrabajo = -1
+	if nodo.AsignarTrabajo(id, img) == true {
+		indexRobin++
 		fmt.Fprint(w, id)
 	} else {
 		fmt.Fprint(w, "-1")
@@ -80,7 +108,6 @@ func handler_estado(w http.ResponseWriter, r *http.Request) {
 	identificador, err := strconv.Atoi(r.URL.Query()["id"][0])
 	if err == nil {
 		fmt.Fprintf(w, "%d", identificador)
-		// fmt.Fprintf(w, "%d", mapatrabajos[identificador])
 	} else {
 		fmt.Fprintf(w, "En marcha", r.URL.Path[1:])
 	}
@@ -94,7 +121,6 @@ func handler_estadisticas(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "En marcha", r.URL.Path[1:])
 	}
 
-	//sd
 }
 
 func main() {
@@ -103,6 +129,9 @@ func main() {
 	*	- Preguntar por foto: /estado
 	*	- Estadísticas: /estadisticas
 	 */
+
+	nodos = list.New()
+
 	http.HandleFunc("/subir", handler_subir)
 	http.HandleFunc("/estado", handler_estado)
 	http.HandleFunc("/estadisticas", handler_estadisticas)
